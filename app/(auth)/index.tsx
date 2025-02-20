fd0c8fcf8658dcbda48e651fdf8af84119911e40
chdrchz <saavannaa.rose@gmail.com>
2025-02-20 10:50:39 -0600

    added auth group, with index.tsx entry point for eventual login/register

---
+++ app/(auth)/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function index() {
+    return (
+        <View>
+            <Text style={{ color: "white" }}>Verifying layout</Text>
+        </View>
+    )
+}
