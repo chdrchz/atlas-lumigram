71dd8487ac5817932b9bb69f607b64b5ad68e746
chdrchz <saavannaa.rose@gmail.com>
2025-02-20 11:08:26 -0600

    tab navigation works

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,9 +1,13 @@
+import { Link } from "expo-router";
 import { View, Text } from "react-native";
 
 export default function index() {
-    return (
-        <View>
-            <Text style={{ color: "white" }}>Verifying layout</Text>
-        </View>
-    )
+  return (
+    <View>
+      <Text style={{ color: "white" }}>Verifying layout</Text>
+      <Link href={"/(tabs)"}>
+        <Text style={{ color: "white" }}>Click me to go to tabs</Text>
+      </Link>
+    </View>
+  );
 }
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -34,9 +34,30 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="search"
         options={{
-          title: 'Explore',
+          title: 'Search',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="add-post"
+        options={{
+          title: 'Create Post',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="favorites"
+        options={{
+          title: 'Favorites',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
